eb9bc94cce3973596e8ce51669ce37b971c782f8
Manju KC <kcmanjuthapa@gmail.com>
2019-10-01 14:50:00 -0400

    created constructor

--- githubuser/src/App.js
+++ githubuser/src/App.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userInfo:[]
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
